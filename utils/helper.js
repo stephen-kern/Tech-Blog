6aa3c3c91a67152f0e55c64fb94aa0b222c0e05f
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-10-17 17:55:39 -0500

    adjustments

--- utils/helper.js
+++ utils/helper.js
@@ -12,11 +12,4 @@ module.exports = {
       .split("/")[0]
       .split("?")[0];
   },
-  format_plural: (word, amount) => {
-    if (amount !== 1) {
-      return `${word}s`;
-    }
-
-    return word;
-  },
 };
